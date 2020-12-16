fc6f292aca4db67f4e388bc4aa3a4c2450b2e375
Antoine Williams <54339074+antoineel9@users.noreply.github.com>
2020-12-15 19:32:55 -0600

    Add files via upload

---
+++ NewClients/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication15.Data;
+
+namespace WebApplication15.Pages.NewClients
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WebApplication15.Data.ApplicationDbContext _context;
+
+        public IndexModel(WebApplication15.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Clients> Clients { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Clients = await _context.Clients.ToListAsync();
+        }
+    }
+}
